d94b6efc4a5391686c7ed9675b1eb459b474db18
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2012-11-07 15:20:23 +0100

    Updated fixtures files and kernel_steps.rb

--- features/step_definitions/kernel_steps.rb
+++ features/step_definitions/kernel_steps.rb
@@ -7,7 +7,7 @@ Given /^I put it through the kernel$/ do
   @tmp_filename = "output_#{rand(1000)}_#{@filename}"
   @output = tmp_file(@tmp_filename)
 
-  `cat #{@input} | ./SimpleTokenizer.py > #{@output}`
+  `cat #{@input} | ./SentimentAggregatorLite.py --no-time > #{@output}`
 end
 
 Then /^the output should match the fixture "(.*?)"$/ do |filename|
